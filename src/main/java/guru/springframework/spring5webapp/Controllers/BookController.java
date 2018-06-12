7ca67e00681d15d5a239746fcac2023cce84bdb4
Kevin <kan@immersion.com>
2018-06-12 17:47:39 -0400

    Add thymeleaf template for author list

--- src/main/java/guru/springframework/spring5webapp/Controllers/BookController.java
+++ src/main/java/guru/springframework/spring5webapp/Controllers/BookController.java
@@ -10,6 +10,7 @@ public class BookController {
 
     private BookRepository bookRepository;
 
+    // autowiring constructor
     public BookController(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
